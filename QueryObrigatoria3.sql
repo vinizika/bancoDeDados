f755009dbaa70e26bde9a38d0851d642e7899c18
Vinícius Duarte <85229756+vinizika@users.noreply.github.com>
2025-04-15 22:12:22 -0300

    Update QueryObrigatoria3.sql

--- QueryObrigatoria3.sql
+++ QueryObrigatoria3.sql
@@ -1,26 +1,26 @@
 WITH disciplinas_cursos AS (
-  SELECT 
-    c.nome AS curso,
+  select 
+    c.nome as curso,
     c.id_curso,
     c.id_departamento,
     cd.id_disciplina
   FROM cursos AS c
-  JOIN curso_disciplina AS cd
+  INNER JOIN curso_disciplina as cd
     ON cd.id_curso = c.id_curso
 )
 SELECT 
   dc.curso,
   d.id_disciplina,
-  d.nome AS disciplina,
-  CAST(d.semestre AS integer) AS semestre
-FROM disciplinas_cursos AS dc
+  d.nome as disciplina,
+  CAST(d.semestre as integer) AS semestre
+FROM disciplinas_cursos as dc
 LEFT JOIN disciplina AS d
-  ON d.id_disciplina = dc.id_disciplina
+  on d.id_disciplina = dc.id_disciplina
 WHERE id_departamento = (
     SELECT id_departamento
     FROM cursos
     GROUP BY id_departamento
-    HAVING COUNT(*) >= 2
+    HAVING count(*) >= 2
     ORDER BY id_departamento
     LIMIT 1
   )
