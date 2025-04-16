d8bf5f6bee861530a1d858cf69d9eb35474f874c
Vinícius Duarte <85229756+vinizika@users.noreply.github.com>
2025-04-15 21:32:44 -0300

    Create QueryObrigatoria3.sql

---
+++ QueryObrigatoria3.sql
@@ -0,0 +1,27 @@
+WITH disciplinas_cursos AS (
+  SELECT 
+    c.nome AS curso,
+    c.id_curso,
+    c.id_departamento,
+    cd.id_disciplina
+  FROM cursos AS c
+  JOIN curso_disciplina AS cd
+    ON cd.id_curso = c.id_curso
+)
+SELECT 
+  dc.curso,
+  d.id_disciplina,
+  d.nome AS disciplina,
+  CAST(d.semestre AS integer) AS semestre
+FROM disciplinas_cursos AS dc
+LEFT JOIN disciplina AS d
+  ON d.id_disciplina = dc.id_disciplina
+WHERE id_departamento = (
+    SELECT id_departamento
+    FROM cursos
+    GROUP BY id_departamento
+    HAVING COUNT(*) >= 2
+    ORDER BY id_departamento
+    LIMIT 1
+  )
+ORDER BY dc.curso, semestre asc
