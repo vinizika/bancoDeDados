de84b951298c593f435ac2a389565cf06e4ec4fa
Vinícius Duarte <85229756+vinizika@users.noreply.github.com>
2025-04-16 19:29:41 -0300

    Create QueryObritaoria3_pt2.sql

---
+++ QueryObritaoria3_pt2.sql
@@ -0,0 +1,31 @@
+WITH disciplinas_cursos AS (
+  SELECT 
+    c.nome AS curso,
+    c.id_curso,
+    c.id_departamento,
+    cd.id_disciplina
+  FROM cursos AS c
+  INNER JOIN curso_disciplina AS cd
+    ON cd.id_curso = c.id_curso
+)
+SELECT 
+  dc.curso,
+  d.id_disciplina,
+  d.nome AS disciplina,
+  CAST(d.semestre AS integer) AS semestre
+FROM disciplinas_cursos AS dc
+LEFT JOIN disciplina AS d
+  ON d.id_disciplina = dc.id_disciplina
+WHERE dc.id_curso = (
+  SELECT MAX(id_curso)
+  FROM cursos
+  WHERE id_departamento = (
+    SELECT id_departamento
+    FROM cursos
+    GROUP BY id_departamento
+    HAVING COUNT(*) >= 2
+    ORDER BY id_departamento
+    LIMIT 1
+  )
+)
+ORDER BY semestre ASC;
